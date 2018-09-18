473ece081a8127bd0b755508cd97da825eaa7a77
ondrejbenes <st39703@student.upce.cz>
2018-09-18 22:23:41 +0200

    Removed TODOs

--- homepage/mailing.py
+++ homepage/mailing.py
@@ -4,10 +4,9 @@ from flask_mail import Message
 from . import app
 
 
-# TODO Async
 def compose_message():
     """Composes a message based on what was entered into the form."""
-    msg = Message('Message from Homepage',  # TODO Add Subject field
+    msg = Message('Message from Homepage',
                   sender=app.config['MAIL_SENDER'],
                   recipients=app.config['MAIL_RECIPIENTS'])
     msg.body = '{name} ({email}) napsal:\n{message}'.format(**dict(request.form.items()))
